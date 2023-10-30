312c6d2699cf61fe8769fb01c0860c4d4a891e2f
Suraj_KY <face.surajyadav@gmail.com>
2023-10-30 15:24:42 +0530

    Xylophone Functionality Completed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var player: AVAudioPlayer!
+    
+    @IBAction func keyPress(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
     }
-
-
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
 }
-
